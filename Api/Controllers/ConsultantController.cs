184dc1c986282b15c01cb4e480328d246dc19faa
rita-ess <rhitaess@gmail.com>
2021-01-01 17:54:57 +0100

    Consultant CRUD done

---
+++ Api/Controllers/ConsultantController.cs
@@ -0,0 +1,71 @@
+﻿using Api.Dao;
+using Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConsultantController : ControllerBase
+    {
+       
+        private readonly IConsultantDao _consultantDao;
+
+
+        public ConsultantController(IConsultantDao consultantDao)
+        {
+            _consultantDao = consultantDao;
+
+        }
+
+        // GET: api/<ConsultantController>
+        [HttpGet]
+        public ActionResult<IEnumerable<Consultant>> Get()
+        {
+            return Ok(_consultantDao.GetAll());
+        }
+
+        // GET api/<ConsultantController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Consultant> Get(int id)
+        {
+            return Ok(_consultantDao.GetById(id));
+        }
+
+        // POST api/<ConsultantController>
+        [HttpPost]
+        public ActionResult<Consultant> Post(Consultant consultant)
+        {
+            return _consultantDao.Store(consultant);
+        }
+
+        // PUT api/<ConsultantController>/5
+        [HttpPut("{id}")]
+        public ActionResult<Consultant> Put(int id, Consultant consultant)
+        {
+            consultant.Id = id;
+            return _consultantDao.Update(consultant);
+        }
+
+        // DELETE api/<ConsultantController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var c = _consultantDao.GetById(id);
+            if (c == null)
+            {
+                return NotFound();
+            }
+
+            if (_consultantDao.Delete(c))
+            {
+                return NoContent();
+            }
+            return null;
+        }
+    }
+}
---
+++ Api/Dao/ConsultantDao.cs
@@ -0,0 +1,50 @@
+﻿using Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Dao
+{
+    public class ConsultantDao : IConsultantDao
+    {
+
+        private readonly AfdtechContext _afdtechContext;
+
+        public ConsultantDao(AfdtechContext afdtechContext)
+        {
+            _afdtechContext = afdtechContext;
+        }
+        public IEnumerable<Consultant> GetAll()
+        {
+            return _afdtechContext.Consultants.ToList();
+        }
+
+        public Consultant GetById(int id)
+        {
+            return _afdtechContext.Consultants.FirstOrDefault(c => c.Id == id);
+
+        }
+
+        public Consultant Store(Consultant consultant)
+        {
+            _afdtechContext.Consultants.Add(consultant);
+            _afdtechContext.SaveChanges();
+            return consultant;
+        }
+
+        public Consultant Update(Consultant consultant)
+        {
+            _afdtechContext.Consultants.Update(consultant);
+            _afdtechContext.SaveChanges();
+            return consultant;
+        }
+
+        public bool Delete(Consultant consultant)
+        {
+            _afdtechContext.Consultants.Remove(consultant);
+            _afdtechContext.SaveChanges();
+            return true;
+        }
+    }
+}

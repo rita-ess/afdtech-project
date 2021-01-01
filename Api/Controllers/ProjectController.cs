5d50fb463279850989df63cb72ff9c5d73cd4d0b
rita-ess <rhitaess@gmail.com>
2021-01-01 17:47:53 +0100

    Project CRUD done

---
+++ Api/Controllers/ProjectController.cs
@@ -0,0 +1,86 @@
+﻿using Api.Dao;
+using Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+
+        private readonly IProjectDao _projectDao;
+
+        public ProjectController(IProjectDao projectDao)
+        {
+            _projectDao = projectDao;
+        }
+
+        // GET: api/<ProjectController>
+        [HttpGet]
+        public ActionResult<IEnumerable<Project>> Get()
+        {
+            return Ok(_projectDao.GetAll());
+        }
+
+        // GET api/<ProjectController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Project> Get(int id)
+        {
+            return Ok(_projectDao.GetById(id));
+        }
+
+        // POST api/<ProjectController>
+        [HttpPost]
+        public ActionResult<Project> Post(Project project)
+        {
+            return _projectDao.Store(project);
+        }
+
+        // PUT api/<ProjectController>/5
+        [HttpPut("{id}")]
+        public ActionResult<Project> Put(int id, Project project)
+        {
+
+         /*   if (_projectDao.GetById(id) == null)
+            {
+                return NotFound();
+            }*/
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+            if (project == null)
+            {
+                return NotFound();
+            }
+            project.Id = id;
+            return _projectDao.Update(project);
+            
+        }
+
+        // DELETE api/<ProjectController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var p = _projectDao.GetById(id);
+            if (p == null)
+            {
+                return NotFound();
+            }
+
+            if (_projectDao.Delete(p))
+            {
+                return NoContent();
+            }
+            return null;
+        }
+    }
+}
---
+++ Api/Dao/IProjectDao.cs
@@ -0,0 +1,17 @@
+﻿using Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Dao
+{
+    public interface IProjectDao
+    {
+        public IEnumerable<Project> GetAll();
+        public Project GetById(int id);
+        public Project Store(Project project);
+        public Project Update(Project project);
+        public Boolean Delete(Project project);
+    }
+}
